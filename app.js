c349753773e868afb9d049cc6c72fa41cbea1adc
seananth <seansafstrom@gmail.com>
2020-05-06 11:17:31 +0200

    Fixed movie search

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+const API = "http://www.omdbapi.com/?t=";
+const TITLE = "guardians+of+the+galaxy";
+const YEAR = "2014";
 const APIKEY = "apikey=c30c46df";
-const API = `http://www.omdbapi.com/?t=guardians+of+the+galaxy&y=2014&${APIKEY}`;
 
 let searchInput = document.querySelector(".searchInput");
 
 document.querySelector(".searchContainer").addEventListener("submit", (e) => {
   e.preventDefault();
 
+  fetch(API + searchInput.value + "&y=" + YEAR + "&" + APIKEY)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+
+
+    });
+
+  
   console.log(searchInput.value);
   document.querySelector(".searchContainer").reset();
 });
-
-fetch(API)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
-
-/*
-let movie = 'guardians of the galaxy';
-
-console.log(movie.split(' ').join('+'));
-*/
